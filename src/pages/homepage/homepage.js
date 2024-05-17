da484f337057f522a85ce4a904bfeec2f0c1b0e8
samragyee-d <samragyeedhakal@gmail.com>
2024-05-17 09:21:03 -0400

    added other card (testing solitaire idea)

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,37 +1,38 @@
 import React from 'react';
 import './homepage.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import ReactCardFlip from "react-card-flip";
 
 function Homepage() {
+
+//add a feature that will duplicate the filler cards
+//add a popup when page is loaded that says "Samragyee Resume" or smth like that. Include a "Play" button.
+//add a deck of cards that the user can draw from. Each card will have a fun fact or smth. 
+//add a page for every section of resume (card text is link)
+//include lil symbols on card (ex. "Education" would have books) similar to real playing cards. 
+//optional: add a point system (idk how this would work yet)
+
 	const [isFlipped, setIsFlipped] = useState(false);
-	
+
 	function flipCover(){
 		setIsFlipped(!isFlipped);
 	};
   return (
-    <main className="book">
+    <main className="board">
 		<ReactCardFlip flipDirection='horizontal' isFlipped={isFlipped}>
 			<div className ='book-cover-front' onClick={flipCover}>
 				<div>
-					<h1>Samragyee Dhakal</h1>
-					<h2>Welcome to my personal website!</h2>
+					<h2>Education</h2>
+					<h2></h2>
 				</div>
 			</div>
 			<div className ='book-cover-back' onClick={flipCover}>
-				<h1>back</h1>
+				<h2>back</h2>
 			</div>
 		</ReactCardFlip>
-		<div className ="interior-cover">
-			<ul className='text'>
-				<h2>Table of Contents</h2>
-				<li>Education</li>
-				<li>Skills</li>
-				<li>Expierence</li>
-				<li>Projects</li>
-				<li>Honors and Awards</li>
-				<li>Misc</li>
-			</ul>
+		<div className="filler-card">
+			<div className="filler-card-int">
+			</div>
 		</div>
     </main>
   );
