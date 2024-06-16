ff05e0662e47800b0117d400eb7ce60c2b7a542b
samragyee-d <samragyeedhakal@gmail.com>
2024-06-16 15:25:14 -0400

    Got button to work

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Homepage from "./pages/homepage/homepage.js";
 import Education from "./pages/Education/Education.js";
-import CardContainer from "./pages/homepage/Cards/CardContainer.js";
+import CardContainer from "./pages/homepage/Cards/CardContainer.js"; 
+import Resume from "./pages/Resume.js";
 import './App.css';
 
 
@@ -16,6 +17,9 @@ function App() {
         </Routes>
         <Routes>
           <Route path="/E" exact Component={Education}/>  
+        </Routes>
+        <Routes>
+          <Route path="/Resume" exact Component={Resume}/>
         </Routes>
       </Router>
     </main>
--- src/pages/homepage/Modal.js
+++ src/pages/homepage/Modal.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import './Modal.css'; 
-import {Link } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
 const Modal = ({ isOpen, onClose }) => {
+  
+
+  const navigate = useNavigate();
+
   if (!isOpen) return null;
+
+  const resumePage = () => {
+    navigate('/Resume');
+  };
 
   return (
     <div className="modal-overlay">
@@ -11,7 +19,7 @@ const Modal = ({ isOpen, onClose }) => {
         <p>To learn more about me, click on the words on the cards.</p>
         <button className="play-button" onClick={onClose}><h2>Start playing: Samragyee's Solitaire</h2></button>
         <p>If you want to see my resume in full, click here:</p>
-        <Link to ="src\pages\homepage\Resume.js"><button className="resume-button"><h2>Resume</h2></button></Link>
+        <button className="resume-button" onClick={resumePage}><h2>Resume</h2></button>
       </div>
     </div>
   );
